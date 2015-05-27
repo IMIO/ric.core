2dcf0bf7b6ec8a1582b9281cf5c5dc8f836aee6b
Stephan Geulette <s.geulette@imio.be>
2015-05-27 15:22:20 +0200

    pep8 corrections

--- ric/core/Extensions/utils.py
+++ ric/core/Extensions/utils.py
@@ -16,10 +16,10 @@ def sge_recent(self, count=10):
     friendly_types.remove('PloneboardForum')
     user = api.user.get_current()
     if user.has_role('Manager') or api.user.has_permission('RIC: Administer website', user=user):
-      for typ in ['directory', 'held_position', 'organization', 'person', 'position']:
-        friendly_types.append(typ)
+        for typ in ['directory', 'held_position', 'organization', 'person', 'position']:
+            friendly_types.append(typ)
     count = int(count)
     return catalog(portal_type=friendly_types,
-                                sort_on='modified',
-                                sort_order='reverse',
-                                sort_limit=count)[:count]
+                   sort_on='modified',
+                   sort_order='reverse',
+                   sort_limit=count)[:count]
