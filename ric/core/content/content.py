d5a25a0fe292b26cfc92cd8250772a95532af78c
Stephan Geulette <s.geulette@imio.be>
2015-05-29 22:28:58 +0200

    Set Title to include orga type

---
+++ ric/core/content/content.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
+from collective.contact.core.content.organization import Organization
+
+
+class RICOrganization(Organization):
+
+    def Title(self):
+        if self.organization_type:
+            factory = getUtility(IVocabularyFactory, 'OrganizationTypesOrLevels')
+            voc = factory(self)
+            voc.getTerm(self.organization_type).title
+            return "%s %s" % (voc.getTerm(self.organization_type).title, self.title)
+        else:
+            return self.title
