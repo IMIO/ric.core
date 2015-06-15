618346d63312bd9dc68e157afc24120063dae5b9
Stephan Geulette <s.geulette@imio.be>
2015-06-15 19:30:48 +0200

    Use organization types vocabulary

--- ric/core/content/content.py
+++ ric/core/content/content.py
@@ -3,14 +3,15 @@
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
 from collective.contact.core.content.organization import Organization
+from collective.contact.core.vocabulary import get_vocabulary, get_directory
 
 
 class RICOrganization(Organization):
 
     def Title(self):
         if self.organization_type:
-            factory = getUtility(IVocabularyFactory, 'OrganizationTypesOrLevels')
-            voc = factory(self)
+            directory = get_directory(self)
+            voc = get_vocabulary(directory.organization_types)
             return "%s %s" % (voc.getTerm(self.organization_type).title, self.title)
         else:
             return self.title
